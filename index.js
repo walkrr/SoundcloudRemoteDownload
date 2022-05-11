d71b92783e75124fa00077cbfe303a75c1be3a3e
walkrr <mason@walkrr.io>
2022-05-11 10:54:37 -0400

    add other files

---
+++ index.js
@@ -0,0 +1,8 @@
+function download(url) {
+    // POST to /download
+}
+
+function drawMetadataForm(metaData) {
+    // Sets the index.html with a form for setting metadata, filled with default values at first
+}
+
---
+++ webApp.py
@@ -0,0 +1,84 @@
+from flask import Flask
+from lyricShit import *
+import json
+import pyexpect
+import youtube_dl
+app = Flask(__name__)
+app._static_folder = ''
+
+@app.route("/")
+def index():
+    return app.send_static_file('index.html')
+
+
+
+@app.route("/downloaded", methods = ['POST'])
+def download():
+    url = 'https://soundcloud.com/not_ac/biscotti-in-the-air-juice-1?utm_source=clipboard'
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+
+    with ydl:
+        result = ydl.extract_info(
+            url,
+            download=True
+        )
+    outFile = result['id'] + result['ext']
+    artist = result['uploader']
+    title = result['title']
+    art = result['thumbnail']
+    # TODO finish download template html and link it here
+    return False
+
+
+
+@app.route("/update", methods =['POST'])
+def update():
+    # TODO updates specified metadata fields for a given path
+    return False
+
+
+
+@app.route("/import", methods =['POST'])
+def beet():
+    path = None
+    try: 
+        runner = pyexpect.spawn("beet import "+path)
+        runner.expect("files")
+        runner.sendline("y")
+    except:
+        return False
+    # do i need to kill the runner? do it if so
+    return True
+
+
+
+@app.route("/embedArt", methods =['POST'])
+def art():
+    # TODO embeds art to a given url
+    return False
+
+
+
+@app.route("/fetchGenuis", methods = ['POST'])
+def genuis():
+    # TODO fetch genuis lyrics for setting up metadata sync
+    return False
+
+
+
+@app.route("/path", methods = ["POST"])
+def path():
+    # TODO return relative path for use with lrc data generator
+    return False
+
+
+
+@app.route("/getData", methods = ["POST"])
+def data(file):
+    # TODO get metadata for specified file
+    return False
+
+
+
+if __name__ == "__main__":
+    app.run()
